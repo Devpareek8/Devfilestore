bdf51d31c5428357a397fb134a8408e28b75d46e
Devpareek8 <165389457+Devpareek8@users.noreply.github.com>
2025-02-07 20:38:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://mrnoffice692:PsO4VGHI9
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://mrnoffice692:PsO4VGHI9heKd7WA@cluster0.o1vcj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = environ.get("DB_URI", "mongodb+srv://dev:devunlimited@dev.tolzi.mongodb.net/?retryWrites=true&w=majority&appName=Dev")
 DB_NAME = environ.get("DB_NAME", "techvjbotz")
 
 # Auto Delete Information
