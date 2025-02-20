523fe7191496e516b3a63f3205f475cdd677cbd7
DEZIINER <127255670+DEZIINER@users.noreply.github.com>
2025-02-20 23:03:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -295,16 +295,22 @@ async def base_site_handler(client, m: Message):
     user = await get_user(user_id)
     cmd = m.command
     text = f"`/base_site (base_site)`\n\n<b>Current base site: None\n\n EX:</b> `/base_site shortnerdomain.com`\n\nIf You Want To Remove Base Site Then Copy This And Send To Bot - `/base_site None`"
+    
     if len(cmd) == 1:
         return await m.reply(text=text, disable_web_page_preview=True)
+    
     elif len(cmd) == 2:
         base_site = cmd[1].strip()
-        if base_site == None:
-            await update_user_info(user_id, {"base_site": base_site})
-            return await m.reply("<b>Base Site updated successfully</b>")
-            
+
+        # Check if user wants to remove the base site fix by xayoonara
+        if base_site.lower() == "none":
+            await update_user_info(user_id, {"base_site": None})
+            return await m.reply("<b>Base Site removed successfully</b>")
+        
+        # Validate the domain
         if not domain(base_site):
             return await m.reply(text=text, disable_web_page_preview=True)
+
         await update_user_info(user_id, {"base_site": base_site})
         await m.reply("<b>Base Site updated successfully</b>")
 
